578ffb362c76e5feb246acecda4445392e32caf8
catalystfour <jake@catalystfour.co.uk>
2021-05-15 18:41:50 +0100

    meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,20 @@ const Index = () => {
   return (
     <div>
       <Head>
-        <title>Daily Giveaways GG</title>
+        <title>Daily Giveaways GG - Free Fortnite Giveaways</title>
+        <meta name="title" content="Daily Giveaways GG - Free Fortnite Giveaways" />
+        <meta name="description" content="We're the #1 place for free gaming giveaways. We give away Fortnite skins every single day" /> 
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://dailygiveaways.gg/" />
+        <meta property="og:title" content="Daily Giveaways GG - Free Fortnite Giveaways" />
+        <meta property="og:description" content="We're the #1 place for free gaming giveaways. We give away Fortnite skins every single day" />
+        <meta property="og:image" content="https://dailygiveaways.gg/img/meta.jpg" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://dailygiveaways.gg/" />
+        <meta property="twitter:title" content="Daily Giveaways GG - Free Fortnite Giveaways" />
+        <meta property="twitter:description" content="We're the #1 place for free gaming giveaways. We give away Fortnite skins every single day" />
+        <meta property="twitter:image" content="https://dailygiveaways.gg/img/meta.jpg" />
+        <meta name="theme-color" content="#7C3AED" />
       </Head>
       <Navbar />
       <div className="container mx-auto mt-10 px-4">
