3eb4534e8061478787b95310f5025ddfc96127db
catalystfour <jake@catalystfour.co.uk>
2021-05-15 18:47:23 +0100

    meta tags

--- pages/[giveawaySlug]/index.tsx
+++ pages/[giveawaySlug]/index.tsx
@@ -47,7 +47,20 @@ const index = ({ giveaway }) => {
     return (
         <div>
             <Head>
-                <title>{giveaway?.name} | Daily Giveaways GG</title>
+            <title>{giveaway.name} - Daily Giveaways GG</title>
+            <meta name="title" content={`${giveaway.name} - Daily Giveaways GG`} />
+            <meta name="description" content="We're the #1 place for free gaming giveaways. We give away Fortnite skins every single day" /> 
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://dailygiveaways.gg/" />
+            <meta property="og:title" content={`${giveaway.name} - Daily Giveaways GG`} />
+            <meta property="og:description" content="We're the #1 place for free gaming giveaways. We give away Fortnite skins every single day" />
+            <meta property="og:image" content="https://dailygiveaways.gg/img/meta.jpg" />
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://dailygiveaways.gg/" />
+            <meta property="twitter:title" content={`${giveaway.name} - Daily Giveaways GG`} />
+            <meta property="twitter:description" content="We're the #1 place for free gaming giveaways. We give away Fortnite skins every single day" />
+            <meta property="twitter:image" content="https://dailygiveaways.gg/img/meta.jpg" />
+            <meta name="theme-color" content="#7C3AED" />
             </Head>
             <Navbar />
             <div className="container mx-auto mt-10 px-4">
