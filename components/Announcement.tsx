faf293cd0c4f4e4a5f11353168b484d14b716de6
catalystfour <jake@catalystfour.co.uk>
2021-05-20 20:50:49 +0100

    announce fix

--- components/Announcement.tsx
+++ components/Announcement.tsx
@@ -9,7 +9,7 @@ export const Announcement = () => {
                 <div className="text-center lg:text-left">What other games should we do giveaways for?</div>
                 <div>
                     <Link href="https://strawpoll.com/u9e47qefx">
-                        <div className="flex justify-center items-center mt-3 lg:mt-0 bg-purple-600 w-full md:w-auto bg-opacity-50 px-4 py-2 rounded-md cursor-pointer">
+                        <div className="flex justify-center items-center mt-3 lg:mt-0 bg-purple-600 w-full md:w-auto px-4 py-2 rounded-md cursor-pointer">
                             Vote here
                             <ExternalLinkIcon className="w-5 h-5 ml-3 opacity-50" />
                         </div>
