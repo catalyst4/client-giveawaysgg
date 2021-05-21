6f88f6babfadc9be529334beb6af7ca551e8593a
catalystfour <jake@catalystfour.co.uk>
2021-05-21 02:46:40 +0100

    announcement fixes

--- components/Announcement.tsx
+++ components/Announcement.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export const Announcement = () => {
     return (
         <div style={{background: '#121212'}} className="w-full px-2 lg:px-5 py-2 mb-2 bg-opacity-20 rounded-lg text-md flex flex-col lg:flex-row justify-between items-center">
-            <div className="text-center lg:text-left">What other games should we do giveaways for?</div>
+            <div className="text-center lg:text-left opacity-80">What other games should we do giveaways for?</div>
             <Link href="https://strawpoll.com/u9e47qefx">
                 <div className="flex justify-center items-center mt-3 lg:mt-0 bg-purple-600 w-full md:w-auto px-4 py-2 rounded-md cursor-pointer">
                     Vote here
