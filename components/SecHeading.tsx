23fb33a18a5a50157f6f70d81d78488a25de79a7
catalystfour <jake@catalystfour.co.uk>
2021-05-24 16:23:18 +0100

    accessibility changes

--- components/SecHeading.tsx
+++ components/SecHeading.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const SecHeading = ({ children }) => {
     return (
-        <h3 className="text-lg mb-1 uppercase font-bold text-purple-500">{children}</h3>
+        <h2 className="text-lg mb-1 uppercase font-bold text-purple-500">{children}</h2>
     )
 }
